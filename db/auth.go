69656ba1e619090937022fe7b5fd5f8e89786ed5
diptanu09 <deb.diptanu09@gmail.com>
2026-07-12 11:39:59 +0530

    Update  VMProtect SDK

--- db/auth.go
+++ db/auth.go
@@ -2,7 +2,7 @@ package db
 
 /*
 // This CGO block tells Go to load the VMProtect SDK
-#cgo CFLAGS: -I../vmp_sdk/Include
+#cgo CFLAGS: -I../vmp_sdk/Include/C/
 #cgo LDFLAGS: -L../vmp_sdk/Lib/Windows -lVMProtectSDK64
 #include "VMProtectSDK.h"
 #include <stdlib.h>
