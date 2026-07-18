f9d76319cd2668d375f8fe944f26e32cdae4ef3f
diptanu09 <deb.diptanu09@gmail.com>
2026-07-18 10:04:48 +0530

    Update

--- db/client.go
+++ db/client.go
@@ -24,11 +24,11 @@ func FetchLatestAppVersion() (string, string, error) {
 	var latestVersion, downloadPath string
 	err := db.QueryRow("SELECT config_value FROM system_config WHERE config_key = 'latest_version';").Scan(&latestVersion)
 	if err != nil {
-		return "2.3.2", "", nil
+		return "2.3.3", "", nil
 	}
 	err = db.QueryRow("SELECT config_value FROM system_config WHERE config_key = 'download_path';").Scan(&downloadPath)
 	if err != nil {
-		return "2.3.2", "", nil
+		return "2.3.3", "", nil
 	}
 	return latestVersion, downloadPath, nil
 }
